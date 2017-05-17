b64c56903b41fc4c7a27a25c2848e87188ceaa6c
Miky <813853090@qq.com>
2017-05-17 17:41:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,32 +2,52 @@ var fs = require('fs');
 var xml2js = require('xml2js');
 var Client = require('ssh2').Client;
 var http = require('http');
+var iconv = require('iconv-lite');
 
 
 var parser = new xml2js.Parser();
 var builder = new xml2js.Builder();
 
 const gxpIp = {
-    dev63: '192.168.0.103'
+    dev63: '10.16.51.63',
+    dev99: '10.20.161.99',
+    dev48: '10.19.131.48',
+    verONE: '10.20.161.103',
+    verTWO: '10.16.51.153',
+    verTHREE: '10.20.161.91',
+    verFOUR: '10.20.161.35'
 }
 
 const cmd = `stopgxp -e\n
              startgxp\n
+             sleep 1\n
              exit\n`;
 
-http.createServer(function (req, res) {
-    req.setEncoding('utf8');
-    var ip = req.url.replace('/', '');
-    if (gxpIp[ip]) {
-        updateConfigure(ip, res);
+// const cmd = `exit\n`;
 
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write('<head><meta charset="utf-8"/></head>');
-        res.end('无效地址');
+http.createServer(function (req, res) {
+    try {
+        req.setEncoding('utf8');
+        var ip = req.url.replace('/', '');
+        if (gxpIp[ip]) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('<head><meta charset="utf-8"/></head>');
+            res.write(`<div><span style="color:blue">还原` + gxpIp[ip] + `环境开始</span></div>`);
+            updateConfigure(ip, res);
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('<head><meta charset="utf-8"/></head>');
+            res.end('无效地址');
+        }
+
+    } catch (e) {
+        console.log('', e, '', e.stack);
+        try {
+            res.end(e.stack);
+        } catch (error) {
+
+        }
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<head><meta charset="utf-8"/></head>');
 }).listen(8888);
 
 function GxpConf() {
@@ -174,9 +194,28 @@ function syncConfig(url, backupContent, updateContent) {
     fs.writeFileSync('./config/' + url + '.xml', finallyContent);
 }
 
-
-
-
+function restartService(conn, res, url) {
+    conn.shell(function (err, stream) {
+        if (err) throw err;
+        res.write(`<div><span style="color:blue">重启服务</span></div>`);
+        var bData = false;
+        stream.on('close', function () {
+            conn.end();
+            res.end(`<span style="color:red">更新环境` + gxpIp[url] + `成功</span>`);
+        }).on('data', function (data) {
+            if (bData == false) {
+                bData = true;
+                stream.write(cmd);
+                stream.end();
+            }
+            data = iconv.decode(data, 'gbk');
+            data = data.replace('\n', '</br>');
+            res.write(`<div style="color:blue">` + data + `</div>`);
+        }).stderr.on('data', function (data) {
+            console.log('stderr: ' + data);
+        });
+    });
+}
 
 function updateConfigure(url, res) {
     var backupContent = fs.readFileSync('./config/' + url + '.xml', 'utf-8');
@@ -188,23 +227,13 @@ function updateConfigure(url, res) {
             if (err) throw err;
             sftp.fastGet('cfg/gxpconfpublic.xml', './gxpconfpublic.xml', function (err, result) {
                 if (err) throw err;
+                res.write(`<div><span style="color:blue">得到配置文件</span></div>`);
                 updateContent = fs.readFileSync('./gxpconfpublic.xml', 'utf-8');
                 syncConfig(url, backupContent, updateContent);
                 sftp.fastPut('./config/' + url + '.xml', 'cfg/gxpconfpublic.xml', function (err, result) {
                     if (err) throw err;
-                    conn.shell(function (err, stream) {
-                        if (err) throw err;
-                        stream.on('close', function () {
-                            conn.end();
-                            res.end(`<span style="color:red">更新环境` + gxpIp[url] + `成功</span>`);
-                        }).on('data', function (data) {
-                            fs.appendFileSync('./syncConfig.log', data + '\n');
-                            res.write(`<div style="color:blue">` + data + `</div>`);
-                        }).stderr.on('data', function (data) {
-                            console.log('stderr: ' + data);
-                        });
-                        stream.end(cmd);
-                    })
+                    res.write(`<div><span style="color:blue">合并配置文件并上传</span></div>`);
+                    restartService(conn, res, url);
                 })
 
             });
@@ -213,6 +242,9 @@ function updateConfigure(url, res) {
         host: gxpIp[url],
         port: 22,
         username: 'gxp',
-        password: 'gxp'
+        password: 'gxp',
+        debug: (info => {
+            // console.log(info);
+        })
     });
 }
